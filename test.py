66ece38743a5a5ee6fa7d9a8f1acd53011efc8ca
DesarguesC <425593160@qq.com>
2023-11-30 13:02:42 +0800

    bg

--- test.py
+++ test.py
@@ -117,7 +117,7 @@ def sidebar_bg(header_bg, sidebar_bg, down_bg):
 
 # class="stChatFloatingInputContainer st-emotion-cache-90vs21 e1d2x3se2"
 side_bg = './assets/sidebar.png'
-header_bg_path = './assets/bgg.png'
+header_bg_path = './assets/mobile.png'
 down_bg = './assets/bg1.png'
 
 sidebar_bg(header_bg_path, side_bg, down_bg)
